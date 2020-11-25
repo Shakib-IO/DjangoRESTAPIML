e1978a3bd9ec0b1dc1b51f37ac6fa979e7712522
Shakib-IO <shakib.khan17@northsouth.edu>
2020-11-25 22:42:57 +0600

    ipynb files added

--- APIProject/Prediction/apps.py
+++ APIProject/Prediction/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
-
+import os
+import sys
+import pandas as pd
+from joblib import load
 
 class PredictionConfig(AppConfig):
     name = 'Prediction'
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    CLASSIFIER_FOLDER = os.path.join(BASE_DIR , 'Prediction/classifier/')
+    CLSSIFIER_FILE =  os.path.join(CLASSIFIER_FOLDER , "IrisPrediction.joblib")
+    classifier = load(CLSSIFIER_FILE)
--- APIProject/Prediction/views.py
+++ APIProject/Prediction/views.py
@@ -33,3 +33,24 @@ class Add_values(APIView):
         response_dict = {"sum" : sum}
         return Response(response_dict , status=status.HTTP_201_CREATED)
 
+# Class based view to predict based on IRIS model
+from .apps import PredictionConfig
+import pandas as pd 
+
+class IRIS_Model_Predict(APIView): 
+    def post(self , request , format = None):
+        data = request.data 
+        keys = []
+        values = []
+        for key in data:
+            keys.append(key)
+            values.append(data[key])
+        
+        X = pd.Series(values).to_numpy().reshape(1 ,-1)
+        loaded_classifier = PredictionConfig.classifier
+        y_pred = loaded_classifier.predict(X)
+        y_pred =  pd.Series(y_pred)
+        target_map = {0: 'setosa' , 1: 'versicolor' , 2:'virginica'}
+        y_pred =  y_pred.map(target_map).to_numpy()
+        response_dict = {"Prediced Iris Species": y_pred[0]}
+        return Response(response_dict, status=200)
